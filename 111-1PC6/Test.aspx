54455703cf5cd4614a9e6b2396ead8c17f155a9d
peterdog123 <peter62101@gmail.com>
2022-10-29 01:32:02 +0800

    6666

---
+++ 111-1PC6/Test.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="Test"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="Test" runat="server">
+        <div>
+            <asp:DropDownList ID="ddl_Test" runat="server"></asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 111-1PC6/Test.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC6
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        string[] s_school = new string[3] { "台科", "北大", "亞東" };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < s_school.Length; i++)
+            {
+                ListItem s_item = new ListItem();
+                s_item.Text = s_item.Value = s_school[i];
+                ddl_Test.Items.Add(s_item);
+            }
+        }
+    }
+}
